89c97c03da8c4f583e85203c207fb33956922381
wukan <wu-kan@163.com>
2024-01-23 10:54:17 +0800

    Update drawdown.py

--- polars_ta/performance/drawdown.py
+++ polars_ta/performance/drawdown.py
@@ -1,5 +1,11 @@
 from polars import Expr
 
 
-def ts_drawdown(close: Expr) -> Expr:
-    return close.cum_max() - close
+def ts_max_drawdown(close: Expr) -> Expr:
+    """最大回撤"""
+    return close - close.cum_max()
+
+
+def ts_max_drawdown_rate(close: Expr) -> Expr:
+    """最大回撤率"""
+    return close / close.cum_max() - 1
