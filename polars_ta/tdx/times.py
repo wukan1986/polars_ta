98eba4e5571e69eb7aa68f9bb806de00ab413de0
伍侃 <wu-kan@163.com>
2025-07-25 14:31:10 +0800

    FROMOPEN优化

--- polars_ta/tdx/times.py
+++ polars_ta/tdx/times.py
@@ -1,4 +1,4 @@
-from datetime import time
+from datetime import time, timedelta
 
 from polars import Expr, when
 
@@ -19,7 +19,7 @@ def FROMOPEN(t: Expr) -> Expr:
     df = pl.DataFrame({'datetime': [
         datetime(2025, 1, 1, 0, 0),
         datetime(2025, 1, 1, 9, 25),
-        datetime(2025, 1, 1, 9, 30),
+        datetime(2025, 1, 1, 9, 30, 57),
         datetime(2025, 1, 1, 9, 31),
         datetime(2025, 1, 1, 10, 0),
         datetime(2025, 1, 1, 13, 0),
@@ -27,22 +27,23 @@ def FROMOPEN(t: Expr) -> Expr:
 
     df = df.with_columns(
         FROMOPEN=FROMOPEN(pl.col('datetime')),
-        FROMOPEN1=FROMOPEN1(pl.col('datetime')),
+        FROMOPEN1=FROMOPEN_1(pl.col('datetime'), 0),
+        FROMOPEN2=FROMOPEN_1(pl.col('datetime'), 60),
     )
 
-    shape: (6, 3)
-    ┌─────────────────────┬──────────┬───────────┐
-    │ datetime            ┆ FROMOPEN ┆ FROMOPEN1 │
-    │ ---                 ┆ ---      ┆ ---       │
-    │ datetime[μs]        ┆ i64      ┆ i64       │
-    ╞═════════════════════╪══════════╪═══════════╡
-    │ 2025-01-01 00:00:00 ┆ 0        ┆ 240       │
-    │ 2025-01-01 09:25:00 ┆ 0        ┆ 1         │
-    │ 2025-01-01 09:30:00 ┆ 1        ┆ 1         │
-    │ 2025-01-01 09:31:00 ┆ 2        ┆ 2         │
-    │ 2025-01-01 10:00:00 ┆ 31       ┆ 31        │
-    │ 2025-01-01 13:00:00 ┆ 121      ┆ 121       │
-    └─────────────────────┴──────────┴───────────┘
+    shape: (6, 4)
+    ┌─────────────────────┬──────────┬───────────┬───────────┐
+    │ datetime            ┆ FROMOPEN ┆ FROMOPEN1 ┆ FROMOPEN2 │
+    │ ---                 ┆ ---      ┆ ---       ┆ ---       │
+    │ datetime[μs]        ┆ i64      ┆ i64       ┆ i64       │
+    ╞═════════════════════╪══════════╪═══════════╪═══════════╡
+    │ 2025-01-01 00:00:00 ┆ 0        ┆ 240       ┆ 240       │
+    │ 2025-01-01 09:25:00 ┆ 0        ┆ 1         ┆ 1         │
+    │ 2025-01-01 09:30:57 ┆ 1        ┆ 1         ┆ 2         │
+    │ 2025-01-01 09:31:00 ┆ 2        ┆ 2         ┆ 3         │
+    │ 2025-01-01 10:00:00 ┆ 31       ┆ 31        ┆ 32        │
+    │ 2025-01-01 13:00:00 ┆ 121      ┆ 121       ┆ 122       │
+    └─────────────────────┴──────────┴───────────┴───────────┘
 
     """
     am = (t.dt.time() - time(9, 29)).dt.total_minutes().clip(0, 120)
@@ -50,12 +51,24 @@ def FROMOPEN(t: Expr) -> Expr:
     return am + pm
 
 
-def FROMOPEN1(t: Expr) -> Expr:
+def FROMOPEN_1(t: Expr, offset: int) -> Expr:
     """返回当前时刻距开盘有多少分钟。范围1~240
 
     用于计算量比
     1. 竞价量比，分母应当为1
     2. 日线数据0~8点时，返回240
+    3. 日线数据9点时，返回1
+
+    Parameters
+    ----------
+    t : Expr
+        时间列
+    offset : int
+        偏移量，单位秒
+
+    Notes
+    -----
+    每根K线结束时，标签是当前时间的50多秒，而结束时时间已经到下以分钟了，所以建议加60秒
 
     """
-    return when(t.dt.time() >= time(9)).then(FROMOPEN(t).clip(1, 240)).otherwise(240)
+    return when(t.dt.time() >= time(8, 45)).then(FROMOPEN(t + timedelta(seconds=offset)).clip(1, 240)).otherwise(240)
