7b6b8790414abe89d4161039670d007955ef5727
wukan <wu-kan@163.com>
2026-01-27 18:36:04 +0800

    算子兼容参数为常量

--- polars_ta/__init__.py
+++ polars_ta/__init__.py
@@ -2,5 +2,7 @@ from typing import Optional
 
 from ._version import __version__
 
+# 默认最小误差值
 TA_EPSILON: float = 1e-8
+# 默认最小样本数量
 MIN_SAMPLES: Optional[int] = None
---
+++ polars_ta/utils/functions.py
@@ -0,0 +1,37 @@
+import inspect
+from functools import wraps
+from inspect import currentframe
+
+import polars as pl
+
+import polars_ta
+
+
+def const_to_expr(func):
+    """将表达式中不合法的常量,改成表达式"""
+
+    def repeat_const(a, p):
+        if p.annotation.__name__ == "Expr" and not isinstance(a, pl.Expr):
+            return pl.repeat(a, pl.len())
+        else:
+            return a
+
+    @wraps(func)
+    def decorated(*args):
+        parameters = inspect.signature(func).parameters
+        args = [repeat_const(a, p) for (n, p), a in zip(parameters.items(), args)]
+        return func(*args)
+
+    return decorated
+
+
+def apply_const_to_expr():
+    """应用常量转表达式功能到所有函数
+
+    对于不合法的表达式参数，可以一定程度上兼容，适用于遗传算法中表达式简化成常量的情况
+    """
+    frame = currentframe().f_back
+    for k, v in frame.f_globals.items():
+        if inspect.isfunction(v):
+            if v.__module__ and v.__module__.startswith(polars_ta.__package__):
+                frame.f_locals[k] = const_to_expr(v)
